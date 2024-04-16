3ce2a2a388526c54d982958822c0a3633ed53763
Santosh <alaa.hariri@outlook.com>
2024-04-16 17:35:18 +0300

    updates"

--- retention_accounting/native_patches.py
+++ retention_accounting/native_patches.py
@@ -3,6 +3,7 @@ from erpnext.accounts.doctype.sales_invoice import sales_invoice
 from retention_accounting.native_overrides import make_tax_gl_entries_purchase_invoice, make_tax_gl_entries_sales_invoice, make_gl_entries, make_gl_entries_purchase_invoice
 from erpnext.accounts import general_ledger
 
+
 purchase_invoice.PurchaseInvoice.make_tax_gl_entries = make_tax_gl_entries_purchase_invoice
 sales_invoice.SalesInvoice.make_tax_gl_entries = make_tax_gl_entries_sales_invoice
 
